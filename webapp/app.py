7b47424abc71152bf6f52ad2f1b7fbfbfd06d618
leonardo <leonardo@uoou.com.br>
2025-05-18 17:03:58 -0300

    Melhorias gerais em tela - Adicionando também SVGs das duas tecnologias

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, send_from_directory
 import subprocess
 
 app = Flask(__name__)
@@ -18,6 +18,10 @@ def send_rabbitmq():
 @app.route('/send-kafka')
 def send_kafka():
     return "Funcionalidade do Apache Kafka ainda não implementada.", 501
+
+@app.route('/media/<path:filename>')
+def media(filename):
+    return send_from_directory('media', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
